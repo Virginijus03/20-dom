3a204a7b8b6b802dbc5196b0116bf65ee5ccc637
Virginijus03 <klimauskas@yahoo.com>
2021-06-02 20:18:47 +0300

    css folderis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+
 const liDOM = document.querySelectorAll('li');
-const ingridientai = [];
+
 
 for (let i = 0; i < liDOM.length; i++) {
-    const ingridientas = liDOM[i].innerText.toUpperCase();
-    ingridientai.push(ingridientas);
+    liDOM[i].classList.add('darzove');
 }
-console.log(ingridientai);
+
+
+/*const liDOM = document.querySelectorAll('li');
+liDOM.classList.add('darzove');
+console.log(liDOM);*/
