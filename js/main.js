3442b00455ab3cde3cdcc171e99c62766a953b6e
Virginijus03 <klimauskas@yahoo.com>
2021-06-02 19:14:46 +0300

    sriuba

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const liDOM = document.querySelectorAll('li');
+const ingridientai = [];
+
+for (let i = 0; i < liDOM.length; i++) {
+    const ingridientas = liDOM[i].innerText.toUpperCase();
+    ingridientai.push(ingridientas);
+}
+console.log(ingridientai);
