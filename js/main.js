ea8fac39c46f189674c1bc40cd127f42a9243767
Virginijus03 <klimauskas@yahoo.com>
2021-06-04 17:02:03 +0300

    sriuba

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 
+const minusDOM = document.querySelector('.minus');
+const plusDOM = document.querySelector('.plus');
+const numberDOM = document.querySelector('.number');
+const resetDOM = document.querySelector('.reset');
+const h1DOM = document.querySelector('h1');
+const h1 = 'Te prasideda žaidynės! 🥳';
+
+minusDOM.addEventListener('click', () => {
+    rezultatas = rezultatas - 1;
+    numberDOM.innerText = rezultatas;
+    h1DOM.innerText = 'Žaidimas progrese';
+})
+
+plusDOM.addEventListener('click', () => {
+    h1DOM.innerText = 'Žaidimas progrese';
+})
+
+resetDOM.addEventListener('click', () => {
+    h1DOM.innerText = h1;
+})
+/*
 const hamburgerDOM = document.querySelector('.hamburger');
 const navDOM = document.querySelector('nav');
 
