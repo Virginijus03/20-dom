215d099eed12ed991e63693eae810b1cf7716df4
Virginijus03 <klimauskas@yahoo.com>
2021-06-04 19:54:19 +0300

    zinutes tekstas pridet atimt su rezultatu

--- js/main.js
+++ js/main.js
@@ -4,22 +4,35 @@ const plusDOM = document.querySelector('.plus');
 const numberDOM = document.querySelector('.number');
 const resetDOM = document.querySelector('.reset');
 const h1DOM = document.querySelector('h1');
+const ulDOM = document.querySelector('ul');
+
 const h1 = 'Te prasideda žaidynės! 🥳';
+const gameInProgressMessage = 'Žaidimas progrese';
+
+ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`;
 
 minusDOM.addEventListener('click', () => {
     numberDOM.innerText = --rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese';
+    h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li> Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
 
 plusDOM.addEventListener('click', () => {
     numberDOM.innerText = ++rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese';
+    h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li> Paspaudei pliusa ir dabartinis rezultatas yra  ${rezultatas}.</li>`;
 })
 
 resetDOM.addEventListener('click', () => {
-    numberDOM.innerText = 0;
+    rezultatas = 0;
+    numberDOM.innerText = rezultatas;
     h1DOM.innerText = h1;
+    ulDOM.innerHTML += `<li> Paspaudei reset ir dabartinis rezultatas yra  ${rezultatas}.</li>`;
 })
+
+
+
+
 /*
 const hamburgerDOM = document.querySelector('.hamburger');
 const navDOM = document.querySelector('nav');
